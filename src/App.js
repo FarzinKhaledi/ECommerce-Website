3f87a98e54e87c6d214e1ea775509678f167d040
Rahwa <rahwafesseha@gmail.com>
2022-08-13 12:46:27 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/"> R&S </a>
+      </header>
+      <main>
+        List Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
